8ad1c63a233c906144f5133fb1d531241b092100
Jaemin-S <20203093@kookmin.ac.kr>
2020-06-28 03:01:40 +0900

    Issue #4

---
+++ calc/jam.py
@@ -0,0 +1,20 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+        d = parse_qs(environ['QUERY_STRING'])
+        a = d.get('a', [''])[0]
+        b = d.get('b', [''])[0]
+	x = 0
+	y = 0
+        if '' not in [a, b]:
+                a, b = int(a), int(b)
+                x = a + b
+                y = a * b
+	x, y = str(x), str(y)
+        response_body = html + "(1) a + b = " + x + "\n(2) a * b = " + y
+        start_response('200 OK', [
+                ('Content-Type', 'text/html'),
+                ('Content-Length', str(len(response_body)))
+        ])
+        return [response_body]
